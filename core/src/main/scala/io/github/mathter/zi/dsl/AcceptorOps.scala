479093400d87217d671aa405df2ee7e5e49deda1
mathter <mathter@mail.ru>
2026-06-10 21:40:00 +0300

    Add AcceptorOpsTest

--- core/src/main/scala/io/github/mathter/zi/dsl/AcceptorOps.scala
+++ core/src/main/scala/io/github/mathter/zi/dsl/AcceptorOps.scala
@@ -1,11 +1,12 @@
 package io.github.mathter.zi.dsl
 
 import io.github.mathter.zi.data.PathMap
+import io.github.mathter.zi.eval.Terminal
 import io.github.mathter.zi.path.Path
 
 implicit class AcceptorOps(x: Acceptor[PathMap]) {
   def by[T](path: Path): Acceptor[T] = x.dsl.by(x, path)
 
-  def update[T](path: Path, source: Source[T]): Source[T] =
+  def update[T](path: Path, source: Source[T]): Source[T] & Terminal =
     this.by(path).from(source)
 }
