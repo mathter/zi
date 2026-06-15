f2757598fe3cc0d4c0a04205722de64ad4a93e10
mathter <mathter@mail.ru>
2026-06-15 20:25:07 +0300

    Add BookInputStreamFactory for test

--- data/src/test/scala/io/github/mathter/zi/data/ext/xml/sax/LoadAndTransformTest.scala
+++ data/src/test/scala/io/github/mathter/zi/data/ext/xml/sax/LoadAndTransformTest.scala
@@ -15,52 +15,6 @@ import javax.xml.transform.stream.StreamSource
 import scala.util.Using
 
 object LoadAndTransformTest {
-  val startBook =
-    """
-      |<book>
-      | <title>Clean Code: A Handbook of Agile Software Craftsmanship</title>
-      | <authors>
-      |   <author>Robert C. Martin</author>
-      | </authors>
-      | <isbn>978-0-13-235088-4</isbn>
-      | <content>""".stripMargin
-
-  val endBook =
-    """
-      | </content>
-      |</book>
-      |""".stripMargin
-
-  val chapter =
-    """
-      |   <chapter>
-      |     <title>Chapter 1: Clean Code</title>
-      |     <date>2026-06-11</date>
-      |     <page>
-      |       <number>1</number>
-      |       <count>50</count>
-      |     </page>
-      |     <description>This chapter devoited to clean code bla bla bla</description>
-      |     <description1>This chapter devoited to clean code bla bla bla</description1>
-      |     <description2>This chapter devoited to clean code bla bla bla</description2>
-      |     <vol>1</vol>
-      |     <content>
-      |      <item>
-      |       <title>Item 1</title>
-      |       <page>
-      |         <number>1</number>
-      |       </page>
-      |      </item>
-      |      <item>
-      |       <title>Item 2</title>
-      |       <page>
-      |         <number>2</number>
-      |       </page>
-      |      </item>
-      |     </content>
-      |   </chapter>
-      |""".stripMargin
-
   def main(args: Array[String]): Unit = {
     val serializer = JsonSerializer()
     val t = new SaxXmlTransformer(SAXParserFactory.newNSInstance())
@@ -78,7 +32,7 @@ object LoadAndTransformTest {
     t.addListener(listener)
 
     val n = 1_000_000
-    Using(gen(n)) {
+    Using(BookInputStreamFactory.inputStream(n)) {
       is =>
         val source = StreamSource(is, "book.xml")
 
@@ -102,46 +56,11 @@ object LoadAndTransformTest {
       result.by("chapter/val").from(dsl.origin.by("chapter/vol")),
       result.by("chapter/content/items").from(
         dsl.origin
-          .by("chapter/content/item")
+          .by("chapter/content/item/title")
+          .as[List[String]]
+          .mapElem(e => e.toLowerCase)
       ),
       dsl.result.from(result)
     )
-  }
-
-  def gen(n: Int): InputStream = {
-    new InputStream {
-      var i = 0
-      var position = 0
-      var bytes: Array[Byte] = null
-
-      override def read(): Int = {
-        var result: Int | Null = null
-
-        while (result == null) {
-          if (bytes != null && position < bytes.length) {
-            result = bytes(position)
-            position += 1
-          } else {
-            result = null
-
-            if (i == 0 && position == 0) {
-              bytes = startBook.getBytes
-            } else if (i < n) {
-              bytes = chapter.getBytes
-              position = 0
-              i += 1
-            } else if (i == n) {
-              bytes = endBook.getBytes
-              position = 0
-              i += 1
-            } else {
-              result = -1
-            }
-          }
-        }
-
-        result.asInstanceOf[Int]
-      }
-    }
   }
 }
